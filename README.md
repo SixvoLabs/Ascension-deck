9dd4f083334ba1e0b41092047ab07335143b3f36
Jay Butera <buterajay@gmail.com>
2017-11-05 16:04:38 -0600

    Done

---
+++ README.md
@@ -0,0 +1,8 @@
+To run, specify the relative path to the country json file as a command line
+arg.
+
+(Notice you don't specify the factbook.json folder)
+
+```
+$ python main.py europe/it.json
+```
--- main.py
+++ main.py
@@ -8,16 +8,16 @@ def dr(data):
 def br(data):
     return float(data['People and Society']['Birth rate']['text'].split()[0])
 
-if len(sys.argv) != 3:
+if len(sys.argv) != 2:
     print('Ugh.. you didn not specify all the arguments. Check the README.')
     sys.exit(0)
 
 path = sys.argv[1]
-country = sys.argv[2]
+#country = sys.argv[2]
 with open('factbook.json/' + path) as f:
     data = json.load(f)
     #print(data['People and Society']['Death rate'])
-    print('\nThis year in', country + ',\n')
+    print('\nThis year in the', data['Government']['Country name']['conventional long form']['text'] + ',\n')
     print('****************\n')
     print( int(br(data)) * 309, 'descended' )
     print( int(dr(data)) * 309, 'ascended' )
