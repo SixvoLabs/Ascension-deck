a3ff2b07f0699caccc9daac8bbcfc40c77840224
Jay Butera <buterajay@gmail.com>
2017-11-05 17:05:40 -0600

    Fix formula

--- main.py
+++ main.py
@@ -16,9 +16,10 @@ path = sys.argv[1]
 #country = sys.argv[2]
 with open('factbook.json/' + path) as f:
     data = json.load(f)
-    #print(data['People and Society']['Death rate'])
+
+    total_pop = int(data['People and Society']['Population']['text'].split()[0].replace(',', ''))
     print('\nThis year in the', data['Government']['Country name']['conventional long form']['text'] + ',\n')
     print('****************\n')
-    print( int(br(data)) * 309, 'descended' )
-    print( int(dr(data)) * 309, 'ascended' )
+    print( int(br(data) * total_pop/1000), 'descended' )
+    print( int(dr(data) * total_pop/1000), 'ascended' )
     print('\n****************\n')
